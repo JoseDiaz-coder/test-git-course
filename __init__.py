6d3f76c485df65348be6cda35cb00a501f9fe52c
José Manuel Diaz <josema.diazh@gmail.com>
2021-02-13 14:45:59 -0600

    Se actualizo alv

--- __init__.py
+++ __init__.py
@@ -1,6 +1,12 @@
-from index import multiply
+from index import multiply, result_add, result_subs
 def prints():
     result = multiply(1,2)
+    print(result)
+
+    result = result_add(1,2)
+    print(result)
+
+    result = result_subs(1,2)
     print(result)
 
 prints()
--- index.py
+++ index.py
@@ -7,14 +7,9 @@ def multiply(a, b):
 
     return result
 
-result = multiply(2,3)
-print(result)
-
 result_add = lambda a, b: a + b
-print(result_add(2,3))
 
 result_subs = lambda a, b: a - b
-print(result_subs(2,3))
 
 
 
