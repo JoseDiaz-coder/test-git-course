5531a5167678326c82d8701f5e38cb574209fb50
José Manuel Diaz <josema.diazh@gmail.com>
2021-02-16 21:02:21 -0600

    se agrego funcion nueva

--- modules.py
+++ modules.py
@@ -1,9 +1,16 @@
 def hello():
     print("hello world")
 
-
 def run():
     print("run run no se cansa mi trokita")
 
 def walk():
     print("yesterday i walked 10 miles and I almost died")
+
+
+def rosario():
+
+    familyTogether = True
+
+    if familyTogether: return True
+    else: return False
